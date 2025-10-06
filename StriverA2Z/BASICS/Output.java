0432b520bd2175b4bfbad6c2623535d264cecfdb
Elfinxd <shivanshdixit9450@gmail.com>
2025-10-06 22:09:55 +0530

    Updated Output

---
+++ StriverA2Z/BASICS/Output.java
@@ -0,0 +1,10 @@
+package StriverA2Z.BASICS;
+import java.util.*;
+public class Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        int a = sc.nextInt();
+        System.out.println("Number is : " + a);
+    }
+}
