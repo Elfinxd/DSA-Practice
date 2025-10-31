ade6d9fa472bcc44aafc46bd664a13ddb8ca6033
Elfinxd <shivanshdixit9450@gmail.com>
2025-10-31 09:58:42 +0530

    Updated Pattern4

---
+++ StriverA2Z/Patterns/Pattern4.java
@@ -0,0 +1,27 @@
+package StriverA2Z.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the no of test cases : ");
+         int t = sc.nextInt();
+        for(int a = 0; a<t; a++)
+        {
+            int n  = sc.nextInt();
+            pattern(n);
+        }
+    }
+    static void pattern(int n)
+    {
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=i; j++)
+            {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        }
+}
