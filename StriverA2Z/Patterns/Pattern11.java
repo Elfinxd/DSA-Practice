218add39b41ad1354af715dc8074242de1fee801
Elfinxd <shivanshdixit9450@gmail.com>
2025-10-31 09:59:51 +0530

    Updated Pattern11

---
+++ StriverA2Z/Patterns/Pattern11.java
@@ -0,0 +1,34 @@
+package StriverA2Z.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the no of test cases : ");
+         int t = sc.nextInt();
+        for(int a = 0; a<t; a++)
+        {
+            int n  = sc.nextInt();
+            pattern(n);
+        }
+    }
+    static void pattern(int n)
+    {
+        int start = 1;
+        for(int i = 0; i<n; i++)
+        {
+            if(i % 2 == 0)
+            start = 1;
+            else
+            start = 0;
+            for(int j = 0; j<=i; j++)
+            {
+                System.out.print(start);
+                start = 1 - start; //start
+            }
+            System.out.println();
+        }
+    }
+    
+}
