c359530cf9ad562d426c87beff4a2bab3b94fddb
Elfinxd <shivanshdixit9450@gmail.com>
2025-11-03 17:34:08 +0530

    Updated Pattern14

---
+++ StriverA2Z/Patterns/Pattern14.java
@@ -0,0 +1,27 @@
+package StriverA2Z.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the no of test cases : ");
+         int t = sc.nextInt();
+        for(int a = 0; a<t; a++)
+        {
+            int n  = sc.nextInt();
+            pattern(n);
+        }
+    }
+    static void pattern(int n)
+    {
+        for(int i = 0; i<n; i++)
+        {
+            for(char ch = 'A'; ch<='A' + i; ch++)
+            {
+                System.out.print(ch);
+            }
+            System.out.println();
+
+                }        }
+}
