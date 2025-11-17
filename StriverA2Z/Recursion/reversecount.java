331fee10c0c63d8cb0cb0b3bcc2a8be9a6571f55
Elfinxd <shivanshdixit9450@gmail.com>
2025-11-17 14:37:30 +0530

    Auto update

--- StriverA2Z/Recursion/reversecount.java
+++ StriverA2Z/Recursion/reversecount.java
@@ -4,6 +4,15 @@ public class reversecount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number : ");
-        int n = sc.nextInt
+        int n = sc.nextInt();
+        Reverse(n);
+    }
+    public static void Reverse(int n)
+    {
+        if(n == 0)
+        return;
+        System.out.print(n + " ");
+        n--;
+        Reverse(n);
     }
 }
