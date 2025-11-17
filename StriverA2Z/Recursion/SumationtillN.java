c526713388e54b62a2e9431acdd1bc8ab833c50f
Elfinxd <shivanshdixit9450@gmail.com>
2025-11-17 17:25:32 +0530

    Auto update

--- StriverA2Z/Recursion/SumationtillN.java
+++ StriverA2Z/Recursion/SumationtillN.java
@@ -5,15 +5,25 @@ public class SumationtillN {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any number : ");
         int n = sc.nextInt();
-        sumationtilln(n, 0);
+       System.out.println( sumationtilln(n));
+       //sumationtilln(n, 0);
     }
-    public static void sumationtilln(int i, int sum)
+    //Paremeterized way 1
+    // public static void sumationtilln(int i, int sum)
+    // {
+    //     if(i<1)
+    //     {
+    //         System.out.println(sum);
+    //         return;
+    //     }
+    //     sumationtilln(i-1, sum+i);
+    // }
+
+    //Functional Way 2
+    public static int sumationtilln(int n) 
     {
-        if(i<1)
-        {
-            System.out.println(sum);
-            return;
-        }
-        sumationtilln(i-1, sum+i);
+        if(n == 0)
+        return 0;
+        return (n + sumationtilln(n-1));
     }
 }
