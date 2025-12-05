4510b90a68e923b17a6a50cc473beda0daca47af
Elfinxd <shivanshdixit9450@gmail.com>
2025-12-05 11:43:33 +0530

    Auto update

---
+++ StriverA2Z/Recursion/reversearray.java
@@ -0,0 +1,26 @@
+package StriverA2Z.Recursion;
+
+public class reversearray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int i = 0;
+        func(arr, i);
+        for(int j = 0; j<arr.length; j++)
+        {
+            System.out.println(arr[j]);
+        }
+    }
+    public static void func(int[] arr, int i)
+    {
+        if(i>=arr.length/2)
+        {
+            return;
+        }
+
+        int temp = arr[arr.length-i-1];
+        arr[arr.length-i-1] = arr[i];
+        arr[i] = temp;
+
+        func(arr, i+1);
+    }    
+}
