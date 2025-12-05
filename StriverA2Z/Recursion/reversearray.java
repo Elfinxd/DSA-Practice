2a0fe78c93c9cc6eb6c9bef36453fc28140c2df1
Elfinxd <shivanshdixit9450@gmail.com>
2025-12-05 11:46:09 +0530

    Added : reversearray.java

--- StriverA2Z/Recursion/reversearray.java
+++ StriverA2Z/Recursion/reversearray.java
@@ -2,7 +2,7 @@ package StriverA2Z.Recursion;
 
 public class reversearray {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int i = 0;
         func(arr, i);
         for(int j = 0; j<arr.length; j++)
