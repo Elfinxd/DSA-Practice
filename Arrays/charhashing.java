6f59ebe4d982372f4897ac10254341ddd8554e85
Elfinxd <shivanshdixit9450@gmail.com>
2025-12-09 20:07:40 +0530

    Added charhashing.java

--- Arrays/charhashing.java
+++ Arrays/charhashing.java
@@ -6,10 +6,10 @@ public class charhashing {
         System.out.println("Enter the  String : ");
         String s = sc.next();
 
-        int[] hash = new int[26];       //pre-compute
+        int[] hash = new int[26];       //pre-compute (256 length in case of not mentioned that all are lower case)
         for(int i = 0; i<s.length(); i++)
         {
-            hash[s.charAt(i) - 'a']++;
+            hash[s.charAt(i) - 'a']++;          //we wont be doing  - 'a' when their are 256 characters instead just hash[s.charAt(i)]++
         }
 
         System.out.println("Enter the no of queries : ");       //fetch
@@ -17,7 +17,7 @@ public class charhashing {
         while(q-->0)
         {
             char c = sc.next().charAt(0);
-            System.out.println("Frequency : " + hash[c - 'a']);
+            System.out.println("Frequency : " + hash[c - 'a']);     //same here we will just print h[c] instead hash[c - 'a'] 
         }
     }
 }
